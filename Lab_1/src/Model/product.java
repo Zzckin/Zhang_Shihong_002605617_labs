829a099724343e5620d6ade1e18f843b178af1d4
Zzckin <zhang.shih@northeastern.edu>
2023-09-15 23:16:02 -0400

    Lab1

---
+++ Lab_1/src/Model/product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Shihong Zhang
+ */
+public class product {
+    
+    private String name;
+    private String descr;
+    private String avialNum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getAvialNum() {
+        return avialNum;
+    }
+
+    public void setAvialNum(String avialNum) {
+        this.avialNum = avialNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
